e0a42b4526ab28d2e7f7b9218805336b22f90646
sj0404k <ksj06030@naver.com>
2023-12-02 17:50:41 +0900

    testboardData 추가

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import BoardPage from './android/app/src/pages/BoardPage';
 const App = () => {
   
   return (
-    <View><MainPage/></View>
+    <View><BoardPage/></View>
   )
 };
 
--- android/app/src/data/BoardData.js
+++ android/app/src/data/BoardData.js
@@ -1,12 +1,12 @@
 // BoardData.js
 export const boardData = [
-  { title: '1', like: 5, visit: 8, time: '2023.11.01' },
-  { title: '2', like: 5, visit: 8, time: '2023.01.01' },
-  { title: '3', like: 5, visit: 8, time: '2023.01.01' },
-  { title: '4', like: 5, visit: 8, time: '2023.01.01' },
-  { title: '5', like: 5, visit: 8, time: '2023.01.01' },
-  { title: '6', like: 5, visit: 8, time: '2023.01.01' },
-  { title: '7', like: 5, visit: 8, time: '2023.01.01' },
-  { title: '8', like: 5, visit: 8, time: '2023.01.01' },
-  { title: '성공이닷!!', like: 5, visit: 8, time: '2023.01.11' },
+  { title: '1', content: "aaaaaa", like: 5, visit: 8, writer: 'aaa', time: '2023.11.01' },
+  { title: '2', content: "bbbbbb", like: 5, visit: 8, writer: 'bbb', time: '2023.01.01' },
+  { title: '3', content: "cccccc", like: 5, visit: 8, writer: 'ccc', time: '2023.01.01' },
+  { title: '4', content: "dddddd", like: 5, visit: 8, writer: 'ddd', time: '2023.01.01' },
+  { title: '5', content: "eeeeee", like: 5, visit: 8, writer: 'eee', time: '2023.01.01' },
+  { title: '6', content: "ffffff", like: 5, visit: 8, writer: 'fff', time: '2023.01.01' },
+  { title: '7', content: "gggggg", like: 5, visit: 8, writer: 'ggg', time: '2023.01.01' },
+  { title: '8', content: "hhhhhh", like: 5, visit: 8, writer: 'hhh', time: '2023.01.01' },
+  { title: '성공이닷!!', content: "iiiiii", like: 5, visit: 8, writer: 'iii', time: '2023.01.11' },
 ];
