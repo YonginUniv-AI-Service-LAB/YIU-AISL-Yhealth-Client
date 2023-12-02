4b8ab5816157442da76fae40d9a7e7c9505a6017
sj0404k <ksj06030@naver.com>
2023-12-02 21:26:46 +0900

    게시판

--- android/app/src/pages/BoardPage.js
+++ android/app/src/pages/BoardPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StyleSheet, Text, ScrollView } from 'react-native';
-
+import { boardData } from '../data/BoardData';
 
 const BoardPage = () => {
   return (
@@ -9,9 +9,31 @@ const BoardPage = () => {
         <View style={styles.titleContainer}>
           <Text style={styles.title}>게시판</Text>
         </View>
-        <View style={styles.row}>
-          <Text >asdfdafdsssfdsfas</Text>
+        <View>
+          {boardData.map((item, index) => (
+            <View key={index}>
+              <View>
+                <Text style={styles.textTitle}>{item.title}</Text>
+                <Text
+                  style={styles.textcontent}
+                  numberOfLines={1}
+                  ellipsizeMode="tail">{item.content.length > 20 ? item.content.substring(0, 20) + '...' : item.content}</Text>
+                <View style={styles.textRow2}>
+                  <View style={styles.textRow}>
+                    <Text style={styles.textcontent2}>{item.writer}</Text>
+                    <Text style={styles.textcontent2} > | </Text>
+                    <Text style={styles.textcontent2}>{item.time}</Text>
+                  </View>
+                  <View style={styles.textRow}>
+                    <Text style={styles.textcontent3}>icon</Text>
+                    <Text style={styles.textcontent3}>{item.like}</Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+          ))}
         </View>
+        <Text style={styles.text}>글작성</Text>
       </View>
     </ScrollView>
   )
@@ -26,14 +48,49 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     alignItems: 'center',
+    padding: 5,
+    borderBottomWidth: 1,
+    borderColor: 'black',
   },
   row: {
     flexDirection: 'row', // 가로로 배치
     padding: 5,
     borderBottomWidth: 1,
     borderColor: 'black',
-  }
+    
+  },
+  textRow: {
+    flexDirection: 'row',
+  },
+  textRow2: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textTitle: {
+    color: 'rgba(0,0,0, 0.90)',
+    fontSize: 17,
+    fontFamily: 'Noto Sans',
+    fontWeight: '700',
+  },
+  textcontent: {
+    color: 'rgba(0,0,0, 0.70)',
+    fontSize: 15,
+    fontFamily: 'Noto Sans',
+    fontWeight: '400',
 
+  },
+  textcontent2: {
+    color: '#767676',
+    fontSize: 15,
+    fontFamily: 'Noto Sans',
+    fontWeight: '400',
+  },
+  textcontent3: {
+    color: 'rgba(0,0,0, 0.50)',
+    fontSize: 15,
+    fontFamily: 'Noto Sans',
+    fontWeight: '900',
+  },
 })
 
 export default BoardPage;
